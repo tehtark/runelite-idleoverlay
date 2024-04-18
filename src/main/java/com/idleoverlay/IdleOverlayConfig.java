ef51927a35c2d5a7ebc6d58c0dbbb84ab69b28c6
Tark <41029975+tehtark@users.noreply.github.com>
2024-04-18 18:30:57 +0100

    Initial project setup

--- src/main/java/com/idleoverlay/IdleOverlayConfig.java
+++ src/main/java/com/idleoverlay/IdleOverlayConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.idleoverlay;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface IdleOverlayConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/idleoverlay/IdleOverlayPluginTest.java
+++ src/test/java/com/idleoverlay/IdleOverlayPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.idleoverlay;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class IdleOverlayPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(IdleOverlayPlugin.class);
 		RuneLite.main(args);
 	}
 }
